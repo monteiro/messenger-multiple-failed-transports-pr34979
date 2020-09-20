25f93a24b5f82a64915222b882b9bad2b3f1332d
Hugo Monteiro <hugo.monteiro@gmail.com>
2020-09-20 17:05:43 +0100

    fetch branch

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 composer install --no-scripts --ignore-platform-reqs
 git clone git@github.com:monteiro/symfony.git
+cd symfony 
+git co remotes/origin/messenger-multiple-failed-transports
+cd ..
 ./symfony/link .
